51487a0b85931803ba0a0d442a35fc67848c5107
hashemJaber <hashemjaber02@gmail.com>
2024-11-18 21:37:54 -0800

    Debugging

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -375,7 +375,7 @@ class CSPMirrorNet(nn.Module):
       for i in range(num_of_base_blocks):
         self.base_blocks.append(BaseConvBlockCSP(input_shape,4)) #TODO FIX/CHANGEME
       """
-      num_of_base_blocks=int(num_of_base_blocks/2)
+      
       print('out channels', num_of_base_blocks)
       self.base_block = BaseConvBlockCSP(input_shape,num_of_base_blocks,stride=stride,verbose=verbose)
       self.verbose=verbose
